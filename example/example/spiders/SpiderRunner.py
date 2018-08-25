028e1d475e2131f6e13696840db9f8fed4a60c09
doleb <dylancet@gmail.com>
2018-08-24 19:36:08 -0700

    Moved SpiderRunner.py file to spiders folder

--- example/example/spiders/SpiderRunner.py
+++ example/example/spiders/SpiderRunner.py
@@ -1,9 +1,9 @@
 import scrapy
 import os
 from scrapy.crawler import CrawlerProcess
-from spiders.Diversity import DiversityCrawler
-from spiders.Rent import RentCrawler
-from spiders.Transport import TransportCrawler
+from Diversity import DiversityCrawler
+from Rent import RentCrawler
+from Transport import TransportCrawler
 
 process = CrawlerProcess()
 process.crawl(TransportCrawler)
